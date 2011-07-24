5db40ecb52812c7a9e520490468ec9e0d470bd35
Markus Ullmann <mail@markus-ullmann.de>
2011-07-24 19:42:20 +0200

    Implemented RFC6122

---
+++ pyfire/tests/test_jid.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+    pyfire.tests.jid
+    ~~~~~~~~~~~~~~~~
+
+    Tests for JID
+
+    :copyright: (c) 2011 by the pyfire Team, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from pyfire.tests import PyfireTestCase
+
+from pyfire.jid import JID
+
+class TestJID(PyfireTestCase):
+
+    def test_parse_full_jid(self):
+        jid = JID("user@host/res")
+        self.assertEqual(jid.local, "user")
+        self.assertEqual(jid.domain, "host")
+        self.assertEqual(jid.resource, "res")
+
+    def test_parse_domain_partial_jid(self):
+        jid = JID("host/res")
+        self.assertEqual(jid.local, None)
+        self.assertEqual(jid.domain, "host")
+        self.assertEqual(jid.resource, "res")
+
+    def test_parse_domain_jid(self):
+        jid = JID("host")
+        self.assertEqual(jid.local, None)
+        self.assertEqual(jid.domain, "host")
+        self.assertEqual(jid.resource, None)
+
+    def test_bare_jid(self):
+        jid = JID("user@host/res")
+        self.assertEqual(jid.bare, "user@host")
+    
+    def test_jid_compare(self):
+        jid1 = JID("user1@host/res")
+        jid2 = JID("user2@host/res")
+        self.assertTrue(jid1 == jid1)
+        self.assertFalse(jid1 == jid2)
+    
+    def test_jid_ip(self):
+        JID("user@127.0.0.1/res")
+        JID("user@fe80::1/res")
+        
---
+++ pyfire/util.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    pyfire.util
+    ~~~~~~~~~~~
+
+    Various helper functions
+
+    :copyright: (c) 2011 by the pyfire Team, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import socket
+
+def is_valid_ipv4(address):
+    try:
+        addr= socket.inet_pton(socket.AF_INET, address)
+    except socket.error:
+        return False
+    return True
+
+def is_valid_ipv6(address):
+    try:
+        addr= socket.inet_pton(socket.AF_INET6, address)
+    except socket.error:
+        return False
+    return True
