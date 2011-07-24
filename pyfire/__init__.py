18a42d913b38e31cf0997cce307014653d6321a7
Markus Ullmann <mail@markus-ullmann.de>
2011-07-24 12:33:03 +0200

    Initial commit

---
+++ pyfire/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    pyfire
+    ~~~~~~
+
+    pyfire is a Python powered XMPP server implementation.
+
+    :copyright: (c) 2011 by the pyfire Team, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+__version__ = '0.1dev'
