8056531cec9cea14340b80882448725f6a430ee3
Jan Boysen <jan@mediaweb-it.net>
2011-07-24 16:02:19 +0200

    added base for parsing XMPP requests

--- pyfire/xmpphandler.py
+++ pyfire/xmpphandler.py
@@ -13,14 +13,28 @@ import SocketServer
 from socket import SHUT_RDWR, timeout
 from time import sleep
 
+from xml import sax
+import xml.etree.ElementTree as ET
+import streamprocessor
+
 class XMPPHandler(SocketServer.BaseRequestHandler):
-  
+
+  def streamhandler(self, attr ):
+    """ handles an incomming stream start """
+    
+  def contenthandler(self, tree ):
+    """ handles an incomming content tree """
+    
   def handle(self):
     """ Starts the handling for a new connection """
     print "New connection from %s:%i" % self.client_address
 
     self.request.settimeout(0.1)
-    
+
+    self.parser = sax.make_parser(['xml.sax.expatreader'])
+    self.handler = streamprocessor.XMPPContentHandler( self.streamhandler, self.contenthandler )
+    self.parser.setContentHandler( self.handler )
+
     try:
       while( 1 ):
 	## main loop
@@ -29,10 +43,9 @@ class XMPPHandler(SocketServer.BaseRequestHandler):
 	  # if not data received the socket seems to have closed so terminate the connection
 	  if not data:
 	    break	  
+	  self.parser.feed( data )
 	except timeout:
 	  pass
-
-	  # do some useful stuff here :)
 
     except:
       raise
@@ -43,4 +56,4 @@ class XMPPHandler(SocketServer.BaseRequestHandler):
   def finish(self):
     """ called upon socket shutdown either from client- or severside """
     print "Connection closed...\n"
-    
+      
