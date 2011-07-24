b394b1dbdb22528e88a1591c7ed8698b88e03e64
Markus Ullmann <mail@markus-ullmann.de>
2011-07-24 16:46:52 +0200

    Rename Handler to connection and make it threaded

--- pyfire/server.py
+++ pyfire/server.py
@@ -10,13 +10,21 @@
 """
 
 import SocketServer
-from xmpphandler import XMPPHandler
+from xmppconnection import XMPPConnection
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
 
 def main():
     # starts the XMPP listener...
     HOST, PORT = "0.0.0.0", 5222
-    server = SocketServer.TCPServer((HOST, PORT), XMPPHandler)
-    server.serve_forever()
+    server = ThreadedTCPServer((HOST, PORT), XMPPConnection)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.shutdown()
 
 if __name__ == '__main__':
     main()
