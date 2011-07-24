48d02d2c9cea083946c68e494309d7597ec2d878
Markus Ullmann <mail@markus-ullmann.de>
2011-07-24 15:05:49 +0200

    Create unit test base class

--- pyfire/tests/__init__.py
+++ pyfire/tests/__init__.py
@@ -8,3 +8,9 @@
     :copyright: (c) 2011 by the pyfire Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
+
+import unittest
+
+class PyfireTestCase(unittest.TestCase):
+    """All our unittests are based on this class"""
+    pass
