1b6e9e4f07a9a7a1e0c44bdc14c6263a057e4a08
Markus Ullmann <mail@markus-ullmann.de>
2011-07-24 13:44:10 +0200

    Add unit test subdirectory

---
+++ pyfire/tests/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    pyfire.tests
+    ~~~~~~~~~~~~
+
+    All unittests live here
+
+    :copyright: (c) 2011 by the pyfire Team, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
