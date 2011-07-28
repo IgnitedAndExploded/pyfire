fde0601b31e376d41eec11fc512648db1aa6d14e
Markus Ullmann <mail@markus-ullmann.de>
2011-07-28 22:50:06 +0200

    move server script as it is only a development server

--- bin/server.py
+++ bin/server.py
@@ -1,20 +1,31 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    pyfire.server
-    ~~~~~~~~~~~~~
+    Sample Server
 
-    This module starts the main TCP listener for XMPP client communication so far.
+    This module starts the main TCP listener for XMPP clients
 
     :copyright: (c) 2011 by the pyfire Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
 
 import SocketServer
-from xmppconnection import XMPPConnection
+from pyfire.xmppconnection import XMPPConnection
+from pyfire.auth.backends import DummyTrueValidator
+from pyfire.auth.registry import AuthHandlerRegistry, ValidationRegistry
+
 
 class XMPPTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     def __init__(self, sockaddr=("localhost", 5222), handler=XMPPConnection):
         SocketServer.TCPServer.__init__(self, sockaddr, handler)
+
+        # init auth backends
+        validation_registry = ValidationRegistry()
+        self.auth_registry = AuthHandlerRegistry(validation_registry)
+
+        validator = DummyTrueValidator()
+        validation_registry.register('dummy', validator)
+
 
 def main():
     # starts the XMPP listener...
