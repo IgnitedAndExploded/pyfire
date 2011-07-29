0b6eee479430ee2bddb6c35467595724ae2a141e
Markus Ullmann <mail@markus-ullmann.de>
2011-07-29 02:22:11 +0200

    Starter on element testing

---
+++ pyfire/tests/elements/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    pyfire.tests.elements
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Make sure elements have nice behaviour
+
+    :copyright: (c) 2011 by the pyfire Team, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
---
+++ pyfire/tests/elements/test_handler.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+    pyfire.tests.elements.test_handler
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Tests for Tag Handler
+
+    :copyright: (c) 2011 by the pyfire Team, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import xml.etree.ElementTree as ET
+import warnings
+
+from pyfire.auth.registry import AuthHandlerRegistry, ValidationRegistry
+from pyfire.auth.backends import DummyTrueValidator
+from pyfire.elements import TagHandler
+from pyfire.tests import PyfireTestCase
+
+
+class MockServer(object):
+    def __init__(self):
+        self.validator_registry = ValidationRegistry()
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            handler1 = DummyTrueValidator()
+            self.validator_registry.register('dummy', handler1)
+        self.auth_registry = AuthHandlerRegistry(self.validator_registry)
+
+
+class MockConnection(object):
+    def __init__(self):
+
+        self.last_element = None
+        self.last_string = None
+        self.strings = []
+        self.server = MockServer()
+
+    def send_element(self, element):
+        self.last_element = element
+        self.strings.append(ET.tostring(element))
+
+    def send_string(self, string):
+        self.last_string = string
+        self.strings.append(string)
+
+
+class MockAttr(dict):
+    def getValue(self, name):
+        return self[name]
+
+
+class TestTagHandler(PyfireTestCase):
+
+    def setUp(self):
+        self.connection = MockConnection()
+        self.taghandler = TagHandler(self.connection)
+
+    def test_stream_start(self):
+        attrs = {
+            'to': 'localhost',
+            'xmlns': 'jabber:client',
+            'xmlns:stream': 'http://etherx.jabber.org/streams',
+            'version': '1.0'
+        }
+        attrs = MockAttr(attrs)
+        self.taghandler.streamhandler(attrs)
+        self.assertTrue(
+            self.connection.strings[0].startswith(
+                """<?xml version="1.0"?><stream:stream from="localhost" id="""))
+        self.assertTrue(
+            self.connection.strings[0].endswith(
+                """" version="1.0" xml:lang="en" xmlns="jabber:client" """ +
+                """xmlns:stream="http://etherx.jabber.org/streams" >"""
+                ))
