d3847db09e17024f91a3d8e444dfbcad3bc888c8
Markus Ullmann <mail@markus-ullmann.de>
2011-07-31 16:39:27 +0200

    Create basic config handling

---
+++ pyfire/configuration.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+    pyfire.configuration
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Holds and reads/writes configuration settings
+
+    :copyright: (c) 2011 by the pyfire Team, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import ConfigParser
+import os.path
+
+config = ConfigParser.SafeConfigParser()
+
+config.add_section('database')
+config.set('database', 'engine', 'sqlite')
+config.set('database', 'name', '')
+config.set('database', 'user', '')
+config.set('database', 'password', '')
+config.set('database', 'host', '')
+config.set('database', 'port', '')
+config.set('database', 'custom_url', '')
+
+config.add_section('listeners')
+config.set('listeners', 'ip', '127.0.0.1')
+config.set('listeners', 'clientport', '5222')
+
+config.add_section('logging')
+config.set('logging', 'global_level', 'ERROR')
+
+config.read(['pyfire.cfg', os.path.expanduser('~/.pyfire.cfg')])
+
+# some handy shortcuts
+get = config.get
+getint = config.getint
+NoOptionError = ConfigParser.NoOptionError
