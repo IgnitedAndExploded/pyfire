a0d5edd6ae7ed21073220f4950891d1bc9a76b18
Markus Ullmann <mail@markus-ullmann.de>
2011-08-07 17:56:47 +0200

    reflect new logger name properly

--- pyfire/stream/sockethandler.py
+++ pyfire/stream/sockethandler.py
@@ -18,7 +18,7 @@ from pyfire.stream import processor
 from pyfire.stream.elements import TagHandler
 from pyfire.stream.errors import StreamError
 
-log = Logger("XMPPSocketHandler")
+log = Logger("stream.socket")
 
 
 class XMPPSocketHandler(SocketServer.BaseRequestHandler):
