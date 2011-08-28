e278f34d22ac77b34583ead08dd640c3dd7dfc3d
Markus Ullmann <mail@markus-ullmann.de>
2011-08-28 17:50:48 +0200

    split content handling method for better readability

--- pyfire/stream/stanzas/__init__.py
+++ pyfire/stream/stanzas/__init__.py
@@ -16,6 +16,7 @@ import xml.etree.ElementTree as ET
 import zmq
 from zmq.eventloop.zmqstream import ZMQStream
 
+from pyfire.auth.sasl import SASLAuthHandler, MalformedRequestError
 import pyfire.configuration as config
 from pyfire.jid import JID
 from pyfire.logger import Logger
@@ -24,6 +25,8 @@ from pyfire.stream.errors import *
 
 log = Logger(__name__)
 
+BIND_NS="urn:ietf:params:xml:ns:xmpp-bind"
+SESSION_NS="urn:ietf:params:xml:ns:xmpp-session"
 
 
 class TagHandler(object):
@@ -38,6 +41,7 @@ class TagHandler(object):
         self.hostname = None
 
         self.authenticated = False
+        self.session_active = False
         self.publisher = get_publisher()
 
     def contenthandler(self, tree):
@@ -52,35 +56,114 @@ class TagHandler(object):
             if tree.tag == "auth":
                 if self.authenticated:
                     raise NotAllowedError
-                registry = self.connection.auth_registry
-                namespace = tree.get('xmlns')
-                handler = registry.request_handler(namespace)
-                handler.process(tree)
-                self.connection.parser.reset()
-                self.jid = JID("@".join([handler.authenticated_user,
-                                         self.hostname]))
-                self.authenticated = True
-                response_element = ET.Element("success")
-                response_element.set("xmlns", namespace)
-                self.send_element(response_element)
+                self.authenticate(tree)
 
-                processed_stanzas = zmq.Context().socket(zmq.SUB)
-                processed_stanzas.connect(self.publisher.sub_url)
-                processed_stanzas.setsockopt(zmq.SUBSCRIBE, str(self.jid))
-
-                self.processed_stream = ZMQStream(processed_stanzas, self.connection.stream.io_loop)
-                self.processed_stream.on_recv(self.send_string)
-
-            elif tree.tag in ["iq", "message", "presence"]:
+            elif tree.tag == "iq":
                 if not self.authenticated:
                     raise NotAuthorizedError
-                stanza = ET.tostring(tree)
-                log.debug("Publishing Stanza for topic %s: %s" % (tree.get("from"),stanza))
-                self.publisher.send(tree.get("from"), stanza)
+                if self.jid.resource is None:
+                    self.set_resource(tree)
+                else:
+                    if not self.session_active:
+                        first_element = tree[0]
+                        if first_element.tag == "session" and \
+                                first_element.get("xmlns") == SESSION_NS:
+                            response_element = ET.Element("iq")
+                            response_element.set("type", "result")
+                            response_element.set("id", tree.get("id"))
+                            session_element = ET.SubElement(response_element, "session")
+                            session_element.set("xmlns", SESSION_NS)
+                            self.send_element(response_element)
+                            log.debug("Sent empty session element")
+                            self.processed_stream.stop_on_recv()
+                            self.processed_stream.on_recv(self.send_string)
+                        else:
+                            self.publish_stanza(tree)
+                    else:
+                        self.publish_stanza(tree)
+
+            elif tree.tag in ["message", "presence"]:
+                if not self.authenticated:
+                    raise NotAuthorizedError
+                self.publish_stanza(tree)
 
         except StreamError, e:
             self.send_string(unicode(e))
             self.connection.stop_connection()
+
+    def publish_stanza(self, tree):
+        stanza = ET.tostring(tree)
+        log.debug("Publishing Stanza for topic %s: %s" % (tree.get("from"),stanza))
+        self.publisher.send(tree.get("from"), stanza)
+
+    def masked_send_string(self, msg):
+        """Unmark waiting for a session element if we received another stanza response"""
+
+        self.session_active = True
+        self.processed_stream.stop_on_recv()
+        self.processed_stream.on_recv(self.send_string)
+        self.send_string(msg)
+
+    def set_resource(self, tree):
+        """Set a resource on our JID"""
+
+        bind_element = tree[0]
+        if tree.get("type") != "set" or \
+                bind_element.tag != 'bind' or\
+                bind_element.get("xmlns") != BIND_NS:
+            raise NotAuthorizedError
+
+        resource_element = bind_element.find("resource")
+        if resource_element is None:
+            # No prefered resource was set, generate one
+            self.jid.resource = uuid.uuid4().hex
+        else:
+            self.jid.resource = resource_element.text
+        if not self.jid.validate():
+            raise BadRequestError
+
+        # Check if given resource is already in use
+        known_jids = get_known_jids()
+        try:
+            known_jids.append(self.jid)
+        except ValueError:
+            raise ConflictError
+        log.info("Bound connection as %s" % str(self.jid))
+
+        # Send registered resource back to client
+        response_element = ET.Element("iq")
+        response_element.set("type", "result")
+        response_element.set("id", tree.get("id"))
+        bind_element = ET.SubElement(response_element, "bind")
+        bind_element.set("xmlns", BIND_NS)
+        jid_element = ET.SubElement(bind_element, "jid")
+        jid_element.text = str(self.jid)
+        self.send_element(response_element)
+
+    def authenticate(self, tree):
+        """Authenticates user for session"""
+
+        # Currently RFC specifies only SASL as supported way of auth'ing
+        handler = SASLAuthHandler()
+        if tree.get('xmlns') != handler.namespace:
+            raise MalformedRequestError
+        handler.process(tree)
+        self.connection.parser.reset()
+        self.jid = JID("@".join([handler.authenticated_user,
+                                 self.hostname]))
+        self.authenticated = True
+        response_element = ET.Element("success")
+        response_element.set("xmlns", handler.namespace)
+        self.send_element(response_element)
+
+        # Subscribe to stanza responses
+        processed_stanzas_socket = zmq.Context().socket(zmq.SUB)
+        processed_stanzas_socket.connect(self.publisher.sub_url)
+        processed_stanzas_socket.setsockopt(zmq.SUBSCRIBE, str(self.jid))
+
+        self.processed_stream = ZMQStream(processed_stanzas_socket,
+                                          self.connection.stream.io_loop)
+        self.processed_stream.on_recv(self.masked_send_string)
 
     def add_auth_options(self, feature_element):
         """Add supported auth mechanisms to feature element"""
